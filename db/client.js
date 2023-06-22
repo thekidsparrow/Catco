aac334561a62c94b1d8556e5867fd48bb51b5a14
connorbowlan <connor@cbowlan.com>
2023-06-21 19:39:15 -0700

    Fixing dbSeed.js.

--- db/client.js
+++ db/client.js
@@ -1,28 +1,33 @@
-// Connect to DB
-const { Client } = require('pg');
+// The code is importing the Client class from the "pg" module, which is a PostgreSQL client for Node.js.
+// The code sets up a connection string for connecting to a PostgreSQL database.
+// There are two connection string options provided: one for Linux/Mac OS (commented out) and one for Windows.
+// A new instance of the Client class is created using the connection string.
+// The client object is exported as the default export of the module.
 
-// change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'catco-website';
+const { Client } = require("pg");
 
-const localhost = `postgres://postgres:admin@localhost:5432/${DB_NAME}`;
+// Uncomment if on Linux/Mac OS
+// const connString = "postgres://postgres:admin@localhost:5432/catco";
 
-const DB_URL =
-  process.env.DATABASE_URL || localhost;
+// Uncomment if on Windows
+const connString = "postgresql://postgres:admin@localhost:5432/catco";
 
-let client;
-
-// github actions client config
-if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-  });
-} else {
-  // local / heroku client config
-  client = new Client(DB_URL);
-}
+const client = new Client(connString);
 
 module.exports = client;
+
+// GitHub Actions Configuration
+// No longer in use 06/21/2023
+// const dbUrl = process.env.DATABASE_URL || connString;
+//
+// if (process.env.CI) {
+//   client = new Client({
+//     host: "localhost",
+//     port: 5432,
+//     user: "postgres",
+//     password: "postgres",
+//     database: "postgres",
+//   });
+// } else {
+//   // local / heroku client config
+// }
--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const client = require('./client');
+const client = require('./client').default;
 const models = require('./models');
 
 module.exports = {
